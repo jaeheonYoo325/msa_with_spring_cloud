64f5abc58c24cf1ed19f03fc2bb89f776443ed38
jaeheonYoo325 <yoojh0325@gmail.com>
2023-02-19 17:00:54 +0900

    2023.02.19 : Spring Cloud Gateway - 프로젝트 생성.

---
+++ apigateway-service/src/test/java/com/springcloud/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
