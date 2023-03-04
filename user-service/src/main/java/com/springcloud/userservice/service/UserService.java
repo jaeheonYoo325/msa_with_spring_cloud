c63d24c7ff28d322b2f5e48b2a7d95b9b33dde01
jaeheonYoo325 <yoojh0325@gmail.com>
2023-03-04 22:37:46 +0900

    2023.03.04 : Users Microservice - loadUserByUsername() 구현.

--- user-service/src/main/java/com/springcloud/userservice/service/UserService.java
+++ user-service/src/main/java/com/springcloud/userservice/service/UserService.java
@@ -2,8 +2,9 @@ package com.springcloud.userservice.service;
 
 import com.springcloud.userservice.dto.UserDto;
 import com.springcloud.userservice.jpa.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto userDto);
 
     UserDto getUserById(String userId);
